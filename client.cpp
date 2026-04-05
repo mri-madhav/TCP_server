de34c3702404a9476930945edaa4fa29b95c47ac
mri-madhav <mridulmadhav05@gmail.com>
2026-04-05 18:58:34 +0530

    added comments

--- client.cpp
+++ client.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
 
-    // 🔹 0. Initialize Winsock
+    //  Initialize Winsock
     WSADATA wsaData;
 
     if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
@@ -17,7 +17,7 @@ int main() {
         return 1;
     }
 
-    // 🔹 1. Create Socket
+    // Create Socket
     SOCKET clientSocket = INVALID_SOCKET;
 
     clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -31,13 +31,13 @@ int main() {
         cout << "socket() is OK!" << endl;
     }
 
-    // 🔹 2. Setup Server Address (localhost:8080)
+    //  Setup Server Address (localhost:8080)
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     InetPton(AF_INET, _T("127.0.0.1"), &serverAddr.sin_addr.s_addr);
     serverAddr.sin_port = htons(8080);
 
-    // 🔹 3. Connect
+    // Connect
     if (connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         cout << "connect() failed: " << WSAGetLastError() << endl;
         closesocket(clientSocket);
@@ -48,7 +48,7 @@ int main() {
         cout << "connect() is OK!" << endl;
     }
 
-    // 🔹 4. Send Message
+    // Send Message
     const char* msg = "Hello Server";
 
     int bytesSent = send(clientSocket, msg, strlen(msg), 0);
@@ -60,7 +60,7 @@ int main() {
         cout << "Message sent: " << msg << endl;
     }
 
-    // 🔹 5. Cleanup
+    //Cleanup
     closesocket(clientSocket);
     WSACleanup();
 
